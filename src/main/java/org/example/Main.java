86f2caa1a592ea9601bc7398803c4e8e615974fb
realize2578 <realize2578@gmail.com>
2026-01-30 16:42:01 +0900

    [feat]: 과제 2,3단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령)");
+        int cnt = 0 ;
 
-        sc.nextLine();
+        while(true){
+            System.out.print("명령)");
+            String cmd = sc.nextLine();
+
+            if(cmd.equals("등록")){
+                System.out.print("명언:");
+                sc.nextLine();
+                System.out.print("작가:");
+                sc.nextLine();
+                cnt++;
+                System.out.println(cnt+"번 명언이 등록되었습니다.");
+            }
+            else break;
+        }
     }
 }
