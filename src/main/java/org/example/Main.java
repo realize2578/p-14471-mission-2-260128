201dd252ac93f123c60f7277a7b98cafffeb5127
realize2578 <realize2578@gmail.com>
2026-01-30 13:07:54 +0900

    [feat]: 과제 1단계

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        System.out.println("명령)");
+
+        sc.nextLine();
+    }
+}
